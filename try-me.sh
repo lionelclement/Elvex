3e2b0d2666de92c73bf29f65698af19673f6de14
Lionel Clément <lionel.clement@labri.fr>
2019-04-17 22:12:10 +0200

    Add files via upload

---
+++ try-me.sh
@@ -0,0 +1,12 @@
+make -j 5
+(cd data/lefff ; make -j 5)
+
+cd data
+
+echo "--------------------------------------------------------"
+
+#../src/elvex -t -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input
+#../src/elvexdebug -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input -xml fr-1.0.3.xml 2> fr-1.0.3.html ; open fr-1.0.3.html
+../src/elvex  -maxCardinal 1000000 -maxUsages 100000 -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input | ../src/elvexpostedition | sed 's/^ *//'
+
+cd ..
