f39f5b25b0cef770201ccbaa94b52ace3e0df7e4
Lionel Clément <lionel.clement@labri.fr>
2019-04-18 22:56:09 +0200

    Add files via upload

--- try-me.sh
+++ try-me.sh
@@ -1,5 +1,5 @@
-make -j 5
-(cd data/lefff ; make -j 5)
+make -s
+(cd data/lefff ; make -s)
 
 cd data
 
@@ -7,6 +7,6 @@ echo "--------------------------------------------------------"
 
 #../src/elvex -t -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input
 #../src/elvexdebug -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input -xml fr-1.0.3.xml 2> fr-1.0.3.html ; open fr-1.0.3.html
-../src/elvex  -maxCardinal 1000000 -maxUsages 100000 -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input | ../src/elvexpostedition | sed 's/^ *//'
+../src/elvex  -maxCardinal 1000000 -maxUsages 100000 -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input | ../src/elvexpostedition |tr '@' "\n" |sed -e 's/^ *//'
 
 cd ..
