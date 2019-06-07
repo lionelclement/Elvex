beeb4f96e856b31d554213dbed88e7c3426d2181
Lionel Clément <lionel.clement@labri.fr>
2019-06-07 12:57:48 +0200

    Update try-me.sh

--- try-me.sh
+++ try-me.sh
@@ -6,7 +6,7 @@ cd data
 echo "--------------------------------------------------------"
 
 #../src/elvex -t -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input
-#../src/elvexdebug -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input -xml fr-1.0.3.xml 2> fr-1.0.3.html ; open fr-1.0.3.html
+#../src/elvexdebug -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input -xml fr-1.0.3.xml 2> fr-1.0.3.html
 #../src/elvex  -maxCardinal 1000000 -maxUsages 100000 -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input | ../src/elvexpostedition |tr '@' "\n" |sed -e 's/^ *//'
 ../src/elvex  -maxCardinal 1000000 -maxUsages 100000 -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon "text [PRED:hello, i:[FORM:\"`whoami`\"], ii:[PRED:see_you_soon, i:[PRED:installer, i:[PRED:_pro, number:sg, person:two], ii:[FORM:\"Elvex\"], tense:recent_past ]]]" | ../src/elvexpostedition |tr '@' "\n" |sed -e 's/^ *//'
 
