f7ea68fb66fa1de1b1489ec1802bd27064ac6da0
Lionel Clément <lionel.clement@labri.fr>
2019-10-12 13:38:54 +0200

    Update try-me.sh

--- try-me.sh
+++ try-me.sh
@@ -27,6 +27,6 @@ echo "--------------------------------------------------------"
 #../src/elvex -t -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input
 #../src/elvexdebug --traceStage -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input -xml fr-1.0.3.xml > fr-1.0.3.html
 #../src/elvex  -maxCardinal 1000000 -maxUsages 100000 -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input | ../src/elvexpostedition |tr '@' "\n" |sed -e 's/^ *//'
-../src/elvex -maxCardinal 1000000 -maxUsages 100000 -maxLength 10000 -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon "text [PRED:letter, i:[FORM:\"`whoami`\", number:sg, gender:ms], ii:[PRED:installer, i:[PRED:_pro, number:pl, person:two], ii:[FORM:\"Elvex\"], tense:recent_past ], iii:[FORM:\"Lionel Clément\"]]" |tr '@' "\n" |../src/elvexpostedition |sed -e 's/^ *//'
+../src/elvex -maxCardinal 1000000 -maxUsages 100000 -maxLength 10000 -compactDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon "letter [PRED:letter, i:[FORM:\"`whoami`\", number:sg, gender:ms], ii:[PRED:installer, i:[PRED:_pro, number:pl, person:two], ii:[FORM:\"Elvex\"], tense:recent_past ], iii:[FORM:\"Lionel Clément\"]]" |tr '@' "\n" |../src/elvexpostedition |sed -e 's/^ *//'
 
 cd ..
