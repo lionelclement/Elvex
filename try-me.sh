5d1a8f0d5310972b5e340dcf7b9515f232aa51b0
Lionel Clément <lionel.clement@labri.fr>
2019-11-28 09:34:46 +0100

    Update try-me.sh

--- try-me.sh
+++ try-me.sh
@@ -25,7 +25,7 @@ cd data
 echo "--------------------------------------------------------"
 
 #../src/elvex -t -compactLexiconDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input
-#../src/elvexdebug --traceStage -compactLexiconDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input -xml fr-1.0.3.xml > fr-1.0.3.html
+#../src/elvexdebug --traceStage -compactLexiconDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input -xml fr-1.0.3.xml > fr-1.0.3.html ; echo 'open fr-1.0.3.html'
 #../src/elvex  -maxCardinal 1000000 -maxUsages 100000 -compactLexiconDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon -inputFile fr-1.0.3.input | ../src/elvexpostedition |tr '@' "\n" |sed -e 's/^ *//'
 ../src/elvex -maxCardinal 1000000 -maxUsages 100000 -maxLength 10000 -compactLexiconDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.3.grammar -lexiconFile fr-1.0.3.local-lexicon "letter [PRED:letter, i:[FORM:\"`whoami`\", number:sg, gender:ms], ii:[PRED:_installer, i:[PRED:__pro, number:pl, person:two], ii:[FORM:\"Elvex\"], tense:recent_past ], iii:[FORM:\"Lionel Clément\"]]" |tr '@' "\n" |../src/elvexpostedition |sed -e 's/^ *//'
 
