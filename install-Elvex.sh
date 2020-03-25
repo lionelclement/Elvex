45d1d4902f8736309fdb5b280282f8e44677b196
Lionel Clément <lionel.clement@labri.fr>
2020-03-25 15:53:15 +0100

    Add files via upload

--- install-Elvex.sh
+++ install-Elvex.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
+sudo apt-get update
+sudo apt-get install -y git bison flex libxml2-dev autoconf
+apt-get autoclean
 if [ ! -d "Elvex" ]; then
-    apt-get update
-    apt-get install -y git bison flex libxml2-dev autoconf
-    apt-get autoclean
-    https://github.com/lionelclement/Elvex.git
-    cd Elvex
-    aclocal
-    automake -af
-    autoconf
-    ./configure
-    make
-    sudo make install
-    cd ..
+    git clone https://github.com/lionelclement/Elvex.git
 fi
+cd Elvex
+aclocal
+automake -af
+autoconf
+./configure
+make
+sudo make install
+cd ..
