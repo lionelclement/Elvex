422efbe9ca4c439e5f59b91500c7b34579a18fbd
Jean Marcel <lionel.clement@labri.fr>
2020-03-30 20:25:26 +0200

    en-1.0.0

--- install-Elvex.sh
+++ install-Elvex.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+# ##################################################
+#
+# ELVEX
+#
+# Copyright 2019 LABRI, 
+# CNRS (UMR 5800), the University of Bordeaux,
+# and the Bordeaux INP
+#
+# Author: 
+# Lionel Clément
+# LaBRI -- Université Bordeaux 
+# 351, cours de la Libération
+# 33405 Talence Cedex - France
+# lionel.clement@labri.fr
+# 
+# This file is part of ELVEX.
+#
+################################################## #
 
 sudo apt-get update
 sudo apt-get install -y git bison flex libxml2-dev autoconf
@@ -13,4 +31,5 @@ autoconf
 ./configure
 make
 sudo make install
+. ./try-me.sh
 cd ..
