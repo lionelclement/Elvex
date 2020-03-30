00e5adb76274f2b0de5141e4a4dcdaaba18cfd9f
Jean Marcel <lionel.clement@labri.fr>
2020-03-30 20:14:01 +0200

    en-1.0.0

--- try-me.sh
+++ try-me.sh
@@ -21,9 +21,9 @@ cd data
 
 echo "--------------------------------------------------------"
 (cd data/lefff ; make -s -j7)
-elvex -compactLexiconDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.4.grammar -lexiconFile fr-1.0.4.local-lexicon "letter [PRED:letter, from:[FORM:\"Lionel Clément\"], to:[FORM:\"`whoami`\", number:sg, gender:ms], body:[PRED:_installer, i:[PRED:__pro, number:pl, person:two], ii:[FORM:\"Elvex\"], tense:recent_past ]]" |tr '@' "\n" | elvexpostedition | sed -e 's/^ *//' -e 's/  */ /g'
+elvex -compactLexiconDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.4.grammar -lexiconFile fr-1.0.4.local-lexicon "letter [PRED:letter, from:[FORM:\"Lionel Clément\"], to:[FORM:\"`whoami`\", number:sg, gender:ms], body:[PRED:_installer, i:[PRED:__pro, number:pl, person:two], ii:[FORM:\"Elvex\"], tense:recent_past ]]" |tr '@' "\n" | elvexpostedition_fr | sed -e 's/^ *//' -e 's/  */ /g'
 
-#elvex -r -compactLexiconDirectory lefff -compactLexiconFile lefff -grammarFile fr-slang.grammar -lexiconFile fr-slang.local-lexicon -inputFile fr-slang.input | ../src/elvexpostedition |tr '@' "\n" |sed -e 's/^ *//' |tr '@' "\n"
+#elvex -r -compactLexiconDirectory lefff -compactLexiconFile lefff -grammarFile fr-slang.grammar -lexiconFile fr-slang.local-lexicon -inputFile fr-slang.input | elvexpostedition_fr |tr '@' "\n" |sed -e 's/^ *//' |tr '@' "\n"
 #elvex -t -compactLexiconDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.4.grammar -lexiconFile fr-1.0.4.local-lexicon -inputFile fr-1.0.4.input
 #elvexdebug --traceShift -compactLexiconDirectory lefff -compactLexiconFile lefff -grammarFile fr-1.0.4.grammar -lexiconFile fr-1.0.4.local-lexicon -inputFile fr-1.0.4.input
 #elvex -grammarFile greg.grammar -lexiconFile greg.lexicon -inputFile greg.input
@@ -31,6 +31,6 @@ elvex -compactLexiconDirectory lefff -compactLexiconFile lefff -grammarFile fr-1
 #elvexdebug --traceInit --traceStage --traceClose --traceShift --traceReduce --traceAction -grammarFile greg.grammar -lexiconFile greg.lexicon -inputFile greg.input
 
 make en-1.0.0.fsa
-elvex -compactLexiconDirectory . -compactLexiconFile en-1.0.0 -grammarFile en-1.0.0.grammar -lexiconFile en-1.0.0.lexicon -inputFile en-1.0.0.input
+elvex -compactLexiconDirectory . -compactLexiconFile en-1.0.0 -grammarFile en-1.0.0.grammar -lexiconFile en-1.0.0.lexicon -inputFile en-1.0.0.input | elvexpostedition_en | sed 's/^ *//'
 
 cd ..
