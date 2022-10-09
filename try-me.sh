28e681cac7714edb910e71a741d45d93e15a242d
Jean Marcel <lionel.clement@labri.fr>
2022-10-09 20:22:38 +0200

    20221009

--- try-me.sh
+++ try-me.sh
@@ -7,13 +7,13 @@
 # CNRS (UMR 5800), the University of Bordeaux,
 # and the Bordeaux INP
 #
-# Author: 
+# Author:
 # Lionel Clément
-# LaBRI -- Université Bordeaux 
+# LaBRI -- Université Bordeaux
 # 351, cours de la Libération
 # 33405 Talence Cedex - France
 # lionel.clement@labri.fr
-# 
+#
 # This file is part of ELVEX.
 #
 ################################################## #
@@ -22,7 +22,7 @@ cd data
 
 #echo "--------------------------------------------------------"
 (cd lefff ; make -s)
-#elvex -compactedLexiconDirectory lefff -compactedLexiconFile lefff -grammarFile fr-1.0.4.grammar -lexiconFile fr-1.0.4.lexicon "\
+#elvex -compactedLexiconDirectory lefff -compactedLexiconFile lefff -rulesFile fr-1.0.4.rules -lexiconFile fr-1.0.4.lexicon "\
 #letter [\
 #       from:[FORM:\"Lionel Clément\"], \
 #       to:[FORM:\"`whoami`\", number:sg, gender:ms], \
@@ -31,18 +31,18 @@ cd data
 #     |tr '@' "\n" | elvexpostedition_fr | sed -e 's/^ *//' -e 's/  */ /g'
 
 #make en-1.1.0.fsa
-#elvex -compactedLexiconDirectory . -compactedLexiconFile en-1.1.0 -grammarFile en-1.1.0.grammar -lexiconFile en-1.1.0.lexicon "\
+#elvex -compactedLexiconDirectory . -compactedLexiconFile en-1.1.0 -rulesFile en-1.1.0.rules -lexiconFile en-1.1.0.lexicon "\
 #Text[PRED:TO_APPRECIATE, i:[PRED:_YOU], ii:[PRED:ELVEX], vtense:future]"\
 # | elvexpostedition_en | sed 's/^ *//'
-#elvexdebug --traceShift --traceInit --traceStage --traceClose --traceReduce --traceAction -compactedLexiconDirectory . -compactedLexiconFile en-1.1.0 -grammarFile en-1.1.0.grammar -lexiconFile en-1.1.0.lexicon 'Text[PRED:_TO_APPRECIATE, i:[PRED:__YOU], ii:[PRED:_ELVEX], vtense:future]'
+#elvexdebug --traceShift --traceInit --traceStage --traceClose --traceReduce --traceAction -compactedLexiconDirectory . -compactedLexiconFile en-1.1.0 -rulesFile en-1.1.0.rules -lexiconFile en-1.1.0.lexicon 'Text[PRED:_TO_APPRECIATE, i:[PRED:__YOU], ii:[PRED:_ELVEX], vtense:future]'
 
-../bin/elvex -compactedLexiconDirectory lefff -compactedLexiconFile lefff -grammarFile fr-slang.grammar -lexiconFile fr-slang.lexicon -inputFile fr-slang.input| ../bin/elvexpostedition_fr | sed 's/^ *//'
-#../bin/elvex -t -V -compactedLexiconDirectory lefff -compactedLexiconFile lefff -grammarFile fr-slang.grammar -lexiconFile fr-slang.lexicon -inputFile fr-slang.input
-#../bin/elvexdebug --traceShift -compactedLexiconDirectory lefff -compactedLexiconFile lefff -grammarFile fr-slang.grammar -lexiconFile fr-slang.lexicon -inputFile fr-slang.input
+../bin/elvex -compactedLexiconDirectory lefff -compactedLexiconFile lefff -rulesFile fr-slang.rules -lexiconFile fr-slang.lexicon -inputFile fr-slang.input| ../bin/elvexpostedition_fr | sed 's/^ *//'
+#../bin/elvex -t -V -compactedLexiconDirectory lefff -compactedLexiconFile lefff -rulesFile fr-slang.rules -lexiconFile fr-slang.lexicon -inputFile fr-slang.input
+#../bin/elvexdebug --traceShift -compactedLexiconDirectory lefff -compactedLexiconFile lefff -rulesFile fr-slang.rules -lexiconFile fr-slang.lexicon -inputFile fr-slang.input
 
-#elvex -compactedLexiconDirectory lefff -compactedLexiconFile lefff -grammarFile fr-1.0.4.grammar -lexiconFile fr-1.0.4.lexicon -inputFile fr-1.0.4.input | elvexpostedition_fr |tr '@' "\n" |sed -e 's/^ *//' |tr '@' "\n"
-##elvex -grammarFile greg.grammar -lexiconFile greg.lexicon -inputFile greg.input
-#elvex -t -grammarFile greg.grammar -lexiconFile greg.lexicon -inputFile greg.input
-#elvexdebug --traceInit --traceStage --traceClose --traceShift --traceReduce --traceAction -grammarFile greg.grammar -lexiconFile greg.lexicon -inputFile greg.input
+#elvex -compactedLexiconDirectory lefff -compactedLexiconFile lefff -rulesFile fr-1.0.4.rules -lexiconFile fr-1.0.4.lexicon -inputFile fr-1.0.4.input | elvexpostedition_fr |tr '@' "\n" |sed -e 's/^ *//' |tr '@' "\n"
+##elvex -rulesFile greg.rules -lexiconFile greg.lexicon -inputFile greg.input
+#elvex -t -rulesFile greg.rules -lexiconFile greg.lexicon -inputFile greg.input
+#elvexdebug --traceInit --traceStage --traceClose --traceShift --traceReduce --traceAction -rulesFile greg.rules -lexiconFile greg.lexicon -inputFile greg.input
 
 cd ..
