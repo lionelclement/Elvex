22020f8c3fea3ed6d4461b498faf7832fb7d7fb3
Jean Marcel <lionel.clement@labri.fr>
2022-10-27 17:07:06 +0200

    Create gdb.ex

---
+++ gdb.ex
@@ -0,0 +1,4 @@
+set args --traceAction --traceInit --traceStage --traceClose --traceShift --traceReduce --traceAction -rulesFile test1.rules -lexiconFile test1.lexicon -inputFile test1.input
+run
+bt
+
