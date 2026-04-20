1408fa6b7cfff114db7f2f15081a294e53936036
Lionel Clement <lionel.clement@u-bordeaux.fr>
2026-04-20 09:46:15 +0200

    update

--- src/item.hpp
+++ src/item.hpp
@@ -56,7 +56,7 @@ private:
     std::vector<class ForestIdentifier *> forestIdentifiers; // forest identifiers
     environmentPtr environment;                              // variable environment
 
-    bool s_id = true, s_ruleId = false, s_rule = false, s_flags = false, s_refs = true,
+    bool s_id = false, s_ruleId = false, s_rule = false, s_flags = false, s_refs = false,
          s_seen = false, s_item = true, s_index = false, s_indexTerms = false, s_terms = false,
          s_ranges = false, s_forestIdentifiers = false, s_inheritedFeatures = true,
          s_inheritedChildFeatures = true, s_synthesizedFeatures = true,
