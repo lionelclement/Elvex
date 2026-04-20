edafe185ba9ae2cf981120d41960b7680dfcd1cf
Lionel Clement <lionel.clement@u-bordeaux.fr>
2026-04-20 10:31:18 +0200

    update

--- elvex/app/Services/ElvexRunner.php
+++ elvex/app/Services/ElvexRunner.php
@@ -447,7 +447,7 @@ class ElvexRunner
                 $output = self::applyPostTransformations($output, $post);
             } else {
                 $traceCmd = implode(' ', $cmdParts)
-                    . ' --traceAll'
+                    . ' --traceStage --trace'
                     . ' > ' . escapeshellarg($traceFile)
                     . ' 2> /dev/null';
 
