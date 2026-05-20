3988af2f9c37e182b2de9dfa5bb95bc7aa68321a
Lionel Clément <lionel.clement@labri.fr>
2026-05-20 10:19:21 +0200

    Add copyright and author information to orderspec.hpp

--- src/orderspec.hpp
+++ src/orderspec.hpp
@@ -1,3 +1,22 @@
+/* **************************************************
+ *
+ * ELVEX
+ *
+ * Copyright 2014-2026 LABRI,
+ * CNRS (UMR 5800), the University of Bordeaux,
+ * and the Bordeaux INP
+ *
+ * Author:
+ * Lionel Clément
+ * LaBRI - Université Bordeaux 
+ * 351, cours de la Libération
+ * 33405 Talence Cedex - France
+ * lionel.clement@u-bordeaux.fr
+ *
+ * This file is part of ELVEX.
+ *
+ ************************************************** */
+
 #ifndef ELVEX_ORDERSPEC_H
 #define ELVEX_ORDERSPEC_H
 
